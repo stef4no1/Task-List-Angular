cd1d565bfea619af91d796418bbc11b02ca3b8a8
Stefano Straga <stefanostraga2k18@gmail.com>
2022-11-29 17:14:41 +0100

    upload mock-task

---
+++ src/app/mock-task.ts
@@ -0,0 +1,29 @@
+import { Task } from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Terminar primer modulo de Angular",
+        day: 'Enero 5 a las 12 de la noche',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Hacer las compras para la cena",
+        day: 'Febrero 1 a las 22',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Investigar sobre MySql",
+        day: 'Enero 5 a las 12 de la noche',
+        reminder: false
+    },
+    {
+        id: 4,
+        text: "Leer un libro",
+        day: 'Diciembre 14 en el transcurso del dia',
+        reminder: true
+    },
+
+]
